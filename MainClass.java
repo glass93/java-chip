738c217a60a841b175fc76bb26495dfe7d44fe88
3jung <hanhelsink@gmail.com>
2020-10-09 02:25:54 +0900

    어디갔어전에꺼

---
+++ MainClass.java
@@ -0,0 +1,22 @@
+package pjtTest;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+	
+		int i = 10;
+		int j;
+		j = 100;
+		
+		i=200;
+		
+		System.out.println("i + j = " + (i+j));
+		
+		i = 50;
+		System.out.println("i = " + i);
+		
+		i = 9700;
+		System.out.println("i = " + i);
+		
+	}
+}
